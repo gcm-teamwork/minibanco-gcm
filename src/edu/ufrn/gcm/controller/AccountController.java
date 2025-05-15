b12a18194f05fba7137b27fa3dbe270098cefbb8
Gabriele Rêgo <gabrielerego45@gmail.com>
2025-05-15 09:50:56 -0300

    criando operação de criar conta

--- src/edu/ufrn/gcm/controller/AccountController.java
+++ src/edu/ufrn/gcm/controller/AccountController.java
@@ -2,7 +2,18 @@ package edu.ufrn.gcm.controller;
 
 import edu.ufrn.gcm.service.AccountService;
 
+
 public class AccountController {
 
     private AccountService service = new AccountService();
+
+    public String createAccount(String number){
+        boolean createWithSucces = service.createAccount(number);
+        if(createWithSucces){
+            return "Conta criada com sucesso!";
+        }else{
+            return "Erro ao criar conta!";
+        }
+    }
+
 }
